338e64e78865eeaf124315ba56d4545457747a23
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-12-10 13:56:36 +0100

    zavrsen prvi cas

--- todo-frontend/src/store/index.js
+++ todo-frontend/src/store/index.js
@@ -1,11 +1,25 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import VueRouter from "vue-router";
+import ListView from "../views/ListView";
 
-Vue.use(Vuex);
+Vue.use(VueRouter);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+//Ovde definisemo nase rute kao objekte, path je putanja u browseru, npr. /items, /posts
+//component je komponenta koja se prikazuje kada se ucita ta ruta.
+const routes = [{
+  path: "/",
+  name: "home",
+  component: ListView
+}];
+
+// Instancira se VueRouter, standardan nacin za uvezivanje routera.
+// Cli sam odradi ovo za nas
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
 });
+
+export default router;
+
+
